c2b9cfd31125545f649e1bc7118b25adacf4d122
Ahadul Islam <ahadul.islam@selisegroup.com>
2024-06-24 14:42:41 +0600

    file organized

--- index.js
+++ index.js
@@ -1,50 +1,52 @@
 const fs = require("fs");
 const path = require("path");
+const sass = require("sass");
 const CleanCSS = require("clean-css");
 
-// Function to minify a single CSS file
-function minifyCSS(inputFilePath, outputFilePath) {
-    return new Promise((resolve, reject) => {
-        fs.readFile(inputFilePath, "utf8", (err, data) => {
+const inputDir = "./scss"; // Directory containing your SCSS files
+const outputDir = "./css"; // Directory to output your minified CSS files
+
+// Ensure the output directory exists
+if (!fs.existsSync(outputDir)) {
+    fs.mkdirSync(outputDir);
+}
+
+// Function to compile SCSS to CSS and then minify it
+const compileAndMinify = (filePath) => {
+    sass.render({ file: filePath }, (err, result) => {
+        if (err) {
+            console.error(`Error compiling ${filePath}:`, err);
+            return;
+        }
+
+        const minified = new CleanCSS().minify(result.css.toString());
+        if (minified.errors.length > 0) {
+            console.error(`Error minifying ${filePath}:`, minified.errors);
+            return;
+        }
+
+        const outputFilePath = path.join(outputDir, path.basename(filePath, ".scss") + ".min.css");
+        fs.writeFile(outputFilePath, minified.styles, (err) => {
             if (err) {
-                reject(`Error reading file ${inputFilePath}: ${err}`);
-                return;
+                console.error(`Error writing ${outputFilePath}:`, err);
+            } else {
+                console.log(`Successfully compiled and minified ${filePath} to ${outputFilePath}`);
             }
-
-            const output = new CleanCSS().minify(data);
-
-            if (output.errors.length > 0) {
-                reject(`Errors during minification of ${inputFilePath}: ${output.errors}`);
-                return;
-            }
-
-            fs.writeFile(outputFilePath, output.styles, "utf8", (err) => {
-                if (err) {
-                    reject(`Error writing file ${outputFilePath}: ${err}`);
-                } else {
-                    resolve(`Minified CSS written to ${outputFilePath}`);
-                }
-            });
         });
     });
-}
+};
 
-// Function to minify multiple CSS files
-async function minifyMultipleCSS(inputFilePaths, outputDirectory) {
-    for (const inputFilePath of inputFilePaths) {
-        const fileName = path.basename(inputFilePath, ".css");
-        const outputFilePath = path.join(outputDirectory, `${fileName}.min.css`);
-        try {
-            const result = await minifyCSS(inputFilePath, outputFilePath);
-            console.log(result);
-        } catch (error) {
-            console.error(error);
-        }
+// Read the input directory and process each SCSS file
+fs.readdir(inputDir, (err, files) => {
+    if (err) {
+        console.error(`Error reading directory ${inputDir}:`, err);
+        return;
     }
-}
 
-// Example usage: list of CSS files to be minified
-const inputFilePaths = ["./thin.css", "./regular.css", "./bold.css", "./fill.css"];
-const outputDirectory = "./";
-
-minifyMultipleCSS(inputFilePaths, outputDirectory);
+    files.forEach((file) => {
+        const filePath = path.join(inputDir, file);
+        if (path.extname(file) === ".scss") {
+            compileAndMinify(filePath);
+        }
+    });
+});
